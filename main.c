6f950ecd84d0f1bb251c743df3d3c42fcc1a6c6c
JamesDASmith <James.DASmith649@gmail.com>
2025-02-18 23:10:54 -0800

    added additional comments, and organized methods

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-int idExists();
-void menu();
-void printPatient();
-void addPatient();
-void viewPatients();
-int searchPatientByName();
-void searchPatients();
-void organizePatientList();
-void dischargePatient();
-void manageDrSchedule();
+// INFORMATION
+/**
+ * The following program was designed to provide the fundamental groundwork for a hospital management system, allowing
+ * users to provide basic services such as building a schedule for doctors, creating patients with unique information
+ * and managing patients information, and discharging when needed.
+ *
+ * The program is controlled through a series of text based menus, using user inputs to navigate between the menu and
+ * the program's various functions.
+ *
+ * @author James Smith
+ * @author Leslie Zhang
+ * @version 1.0
+ */
 
+// METHOD DECLARATIONS
+int     idExists();
+void    menu();
+void    printPatient();
+void    addPatient();
+void    viewPatients();
+int     searchPatientByName();
+void    searchPatients();
+void    organizePatientList();
+void    dischargePatient();
+void    manageDrSchedule();
+
+// VARIABLE DECLARATIONS
 int totalPatients = 0;
 
 const int DAYS_OF_WEEK = 7;
@@ -28,12 +44,61 @@ const int DELETE = 666;
 
 char * schedule[7][3];
 
+// MAIN METHOD
+/**
+ * Main method. Beginning of the flow of control.
+ *
+ * @return int 0 if program has terminated successfully.
+ */
 int main(void)
 {
     menu();
     return 0;
 }
 
+// PRIMARY METHODS
+/**
+ * Represents the main menu from which a user can choose to use any of the programs functions.
+ */
+void menu() {
+    int choice;
+    do {
+        printf("\nHospital Management System\n");
+        printf("1. Add Patient Record\n");
+        printf("2. View All Patients\n");
+        printf("3. Search Patient by ID\n");
+        printf("4. Discharge Patient\n");
+        printf("5. Manage Doctor Schedule\n");
+        printf("6. Exit\n"
+               "Enter your choice: ");
+
+        scanf("%d", &choice);
+        getchar(); // Consume newline
+
+        switch (choice) {
+            case 1:
+                addPatient();
+            break;
+            case 2:
+                viewPatients();
+            break;
+            case 3:
+                searchPatients();
+            break;
+            case 4:
+                dischargePatient();
+            break;
+            case 5:
+                manageDrSchedule();
+            case 6:
+                printf("Exiting...\n");
+            break;
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    } while (choice != 6);
+    printf("Thanks for visiting!");
+}
 
 //struct representing each patient
 struct Patient {
@@ -47,29 +112,9 @@ struct Patient {
 struct Patient patients[50];
 
 
-int idExists(int id, struct Patient patients[], int size) {
-    for (int i = 0; i < size; i++ ) {
-        if (patients[i].patient_id == id) {
-            return i;
-        }
-    }
-        return -1;
-}
-
-void printPatient(int index) {
-    printf("Patient ID:");
-    printf("%d\n", patients[index].patient_id);
-    printf("Name:");
-    printf("%s\n", patients[index].name);
-    printf("Age:");
-    printf("%d\n", patients[index].age);
-    printf("Diagnosis:");
-    printf("%s\n", patients[index].diagnosis);
-    printf("Room Number:");
-    printf("%d\n", patients[index].room_number);
-}
-
-//enter patients based on user input
+/**
+ * Creates a patient, prompting a user for input.
+ */
 void addPatient() {
     int id;
     char name[50];
@@ -80,11 +125,13 @@ void addPatient() {
 
     struct Patient patient = {};
 
+    // Checks room for more patients at the hospital
     if (totalPatients >= MAX_PATIENTS) {
         printf("Patient Regsitry Full.");
         return;
     }
 
+    // Ensures patient ID is successfully input, continues to prompt in case of invalid input.
     do {
         printf("Enter Patient ID: \n");
         scanf("%d", &id);
@@ -100,6 +147,7 @@ void addPatient() {
     }while (status == 0);
     status = 0;
 
+    // Ensures patient Name is successfully input, continues to prompt in case of invalid input.
     do {
         printf("Enter Patient Name: \n");
         fgets(patient.name, 50, stdin);
@@ -114,6 +162,7 @@ void addPatient() {
 
     status = 0;
 
+    // Ensures patient Age is successfully input, continues to prompt in case of invalid input.
     do {
         printf("Enter Age: \n");
         scanf("%d", &age);
@@ -128,6 +177,7 @@ void addPatient() {
     }while (status == 0);
     status = 0;
 
+    // Ensures patient Diagnosis is successfully input, continues to prompt in case of invalid input.
     do {
         printf("Enter Diagnosis: \n");
         fgets(patient.diagnosis, 50, stdin);
@@ -141,6 +191,7 @@ void addPatient() {
     }while (status == 0);
     status = 0;
 
+    // Ensures patient Room Number is successfully input, continues to prompt in case of invalid input.
     do {
         printf("Enter Room Number: \n");
         scanf("%d", &room_number);
@@ -153,11 +204,15 @@ void addPatient() {
         }
     }while (status == 0);
 
+    // Adds the new patient to the array.
     patients[totalPatients] = patient;
     totalPatients++;
     printf("Patient Added Successfully\n");
 }
 
+/**
+ * Prints the list of patients and their information
+ */
 void viewPatients() {
     printf("Printing Patient Information...\n");
     for (int i = 0; i < totalPatients; i++) {
@@ -169,6 +224,63 @@ void viewPatients() {
     printf("--------------------------\n");
 }
 
+/**
+ * Allows user to search for a patient by either their ID or by Name, printing their information if found.
+ */
+void searchPatients() {
+    int selection;
+    int id;
+    int index;
+    char name[50];
+
+    printf("1. Search Patient By ID\n");
+    printf("2. Search Patient By Name\n");
+    scanf("%d", &selection);
+    getchar();
+
+    switch (selection) {
+        case 1:
+            printf("Enter Patient ID:");
+        scanf("%d", &id);
+        index = idExists(id, patients, totalPatients);
+        if (index != -1) {
+            printf("--------------------------\n");
+            printPatient(index);
+            printf("--------------------------\n");
+        }else {
+            printf("No patients associated with ID %d.\n", id);
+        }
+        break;
+        case 2:
+            printf("Enter Patient Name:");
+        fgets(name, 50, stdin);
+        name[strcspn(name, "\n")] = 0;
+        index = searchPatientByName(name, patients, totalPatients);
+        if (index == -1) {
+            printf("No patients with the name %s exists.\n", name);
+        }else {
+            printf("--------------------------\n");
+            printPatient(index);
+            printf("--------------------------\n");
+        }
+        break;
+        default:
+            printf("Invalid Input. Terminating...\n");
+        break;
+    }
+
+}
+
+/**
+ * Allows the user to search by name if a patient is checked into the hospital. If the patient does exist, the function
+ * returns the index they can be found in the array. Else -1 is returned.
+ *
+ * @param name          - array of chars representing a patient's name.
+ * @param patients      - struct representing an array of patients.
+ * @param totalPatients - int representing the number of patients currently checked into the hospital.
+ *
+ * @return              - int representing the integer representing the index a patient can be found.
+ */
 int searchPatientByName(char name[], struct Patient patients[], int totalPatients) {
     if (strlen(name) == 0) {
         printf("name cannot be empty.\n");
@@ -184,58 +296,9 @@ int searchPatientByName(char name[], struct Patient patients[], int totalPatient
 
 }
 
-void searchPatients() {
-    int selection;
-    int id;
-    int index;
-    char name[50];
-
-    printf("1. Search Patient By ID\n");
-    printf("2. Search Patient By Name\n");
-    scanf("%d", &selection);
-    getchar();
-
-    switch (selection) {
-        case 1:
-            printf("Enter Patient ID:");
-            scanf("%d", &id);
-            index = idExists(id, patients, totalPatients);
-            if (index != -1) {
-                printf("--------------------------\n");
-                printPatient(index);
-                printf("--------------------------\n");
-            }else {
-                printf("No patients associated with ID %d.\n", id);
-            }
-            break;
-        case 2:
-            printf("Enter Patient Name:");
-            fgets(name, 50, stdin);
-            name[strcspn(name, "\n")] = 0;
-            index = searchPatientByName(name, patients, totalPatients);
-            if (index == -1) {
-                printf("No patients with the name %s exists.\n", name);
-            }else {
-                printf("--------------------------\n");
-                printPatient(index);
-                printf("--------------------------\n");
-            }
-            break;
-        default:
-            printf("Invalid Input. Terminating...\n");
-            break;
-    }
-
-}
-
-void organizePatientList(int index, struct Patient patients[], int totalPatients) {
-    struct Patient def = {};
-    for (int i = index; i < totalPatients - 1; i++  ) {
-        patients[i] = patients[i + 1];
-    }
-    patients[totalPatients - 1] = def;
-}
-
+/**
+ * Prompts the user for information about a patient (either name or ID) and removes them from the patient list.
+ */
 void dischargePatient() {
     int choice;
     int id;
@@ -243,13 +306,14 @@ void dischargePatient() {
     int index;
     struct Patient def = {};
 
+    // Menu
     printf("Discharge Patient.\n");
     printf("1. Discharge By ID.\n");
     printf("2. Discharge By Name.\n");
     scanf("%d", &choice);
     getchar();
     switch (choice) {
-        case 1:
+        case 1: // Checks Patient ID, removing them if found.
             printf("Enter ID:\n");
             scanf("%d", &id);
             index = idExists(id, patients, totalPatients);
@@ -262,7 +326,8 @@ void dischargePatient() {
                 printf("Patient with ID %d does not exist.\n", id);
             }
             break;
-        case 2:
+
+        case 2: // Checks Patient Name, removing them if found.
             printf("Enter Name: \n");
             fgets(name, 50, stdin);
             name[strcspn(name, "\n")] = 0;
@@ -283,26 +348,32 @@ void dischargePatient() {
 
 }
 
+/**
+ * Prompts users to various options to either add a doctor to the schedule, or view schedule
+ */
 void manageDrSchedule() {
     int choice;
 
+    // Menu
     printf("What would you like to do?\n");
     printf("1. Add Doctor to Schedule\n");
     printf("2. View Schedule\n");
 
     scanf("%d", &choice);
-    getchar();
+    getchar(); // Remove newline
 
     switch (choice) {
-        case 1:
+        case 1: // User chooses to add a doctor to the schedule.
             char name[50];
             int shiftDay;
             int shiftTime;
 
+            // User inputs Doctor Name
             printf("Enter Doctor Name:\n");
             fgets(name, sizeof(name), stdin);
             name[strcspn(name, "\n")] = 0;
 
+            // User chooses day of said doctor's shift
             printf("Enter Shift Day:\n");
             printf("1. Monday\n");
             printf("2. Tuesday\n");
@@ -319,6 +390,7 @@ void manageDrSchedule() {
                 break;
             }
 
+            // User chooses time of said doctor's shift
             printf("Enter Shift Time:\n");
             printf("1. Morning\n");
             printf("2. Evening\n");
@@ -334,7 +406,7 @@ void manageDrSchedule() {
             schedule[shiftDay][shiftTime] = strdup(name);
             break;
 
-        case 2:
+        case 2: // User chooses to view doctor schedule.
             // Prints the Header for the Calendar
             printf("Shift Calendar\n");
             printf("\t\t\tMorning:\tEvening:\tNight:\n");
@@ -375,42 +447,61 @@ void manageDrSchedule() {
 }
 
 
-void menu() {
-    int choice;
-    do {
-        printf("\nHospital Management System\n");
-        printf("1. Add Patient Record\n");
-        printf("2. View All Patients\n");
-        printf("3. Search Patient by ID\n");
-        printf("4. Discharge Patient\n");
-        printf("5. Manage Doctor Schedule\n");
-        printf("6. Exit\n"
-               "Enter your choice: ");
-
-        scanf("%d", &choice);
-        getchar(); // Consume newline
-
-        switch (choice) {
-            case 1:
-                addPatient();
-            break;
-            case 2:
-                viewPatients();
-            break;
-            case 3:
-                searchPatients();
-            break;
-            case 4:
-                dischargePatient();
-            break;
-            case 5:
-                manageDrSchedule();
-            case 6:
-                printf("Exiting...\n");
-            break;
-            default:
-                printf("Invalid choice! Try again.\n");
+// SUPPORT METHODS
+/**
+ * Checks if a passed in patient ID exists in the system.
+ * If it does the index of the patient is returned, else -1 is returned.
+ *
+ * @param id        - the ID you would like to check to see if it exists in the system.
+ * @param patients  - the struct representing the array of patients in the system.
+ * @param size      - the size of the patient array the method will check through.
+ * @return int      - representing the index of the patient whose ID was passed into the method. If no patient is found
+ *                  then -1 is returned instead.
+ */
+int idExists(int id, struct Patient patients[], int size) {
+    for (int i = 0; i < size; i++ ) {
+        if (patients[i].patient_id == id) {
+            return i;
         }
-    } while (choice != 6);
-    printf("Thanks for visiting!");
+    }
+    return -1;
 }
+
+/**
+ * Organizes the list of patients in the hotel.
+ *
+ * @param index         - index of an incoming patient in the array
+ * @param patients      - struct representing a list of patients
+ * @param totalPatients - int representing the total number of patients checked into the hotel
+ */
+void organizePatientList(int index, struct Patient patients[], int totalPatients) {
+    struct Patient def = {};
+    for (int i = index; i < totalPatients - 1; i++  ) {
+        patients[i] = patients[i + 1];
+    }
+    patients[totalPatients - 1] = def;
+}
+
+/**
+ * Prints the information of the patient found at the passed in index.
+ * This information includes their: ID,
+ *                                  Name,
+ *                                  Age,
+ *                                  Diagnosis, and
+ *                                  Room Number.
+ *
+ * @param index int - representing the index of the patient's location in the struct array.
+ */
+void printPatient(int index) {
+    printf("Patient ID:");
+    printf("%d\n", patients[index].patient_id);
+    printf("Name:");
+    printf("%s\n", patients[index].name);
+    printf("Age:");
+    printf("%d\n", patients[index].age);
+    printf("Diagnosis:");
+    printf("%s\n", patients[index].diagnosis);
+    printf("Room Number:");
+    printf("%d\n", patients[index].room_number);
+}
+
